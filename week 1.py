cf973501ca6d449d497b3ab7d9aeab68e9f05062
VINAY SINGH CHAUDHARY <136018031+VinaySinghChaudhary1@users.noreply.github.com>
2023-06-09 01:22:23 +0530

    Add files via upload

---
+++ week 1.py
@@ -0,0 +1,240 @@
+
+##    WEEK 01     
+
+#..........Class 12
+s='good'
+print(s*5)
+print(s[0]*5)
+
+x="India"
+print(x=='India')
+print(x=='india')
+
+print('apple'>'one')   #computer compare by letter position by position 
+print('four'<'ten')
+print('ap'>'on')     # p come later in alphanbate than n
+print('abcdef'>'abcde')
+
+
+print('hii.............')
+
+s = 'VinaySingh'
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+print(s[6])
+print(s[7])
+print(s[8])
+print(s[9])
+
+print('hii.............')
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+print(s[-6])
+print(s[-7])
+print(s[-8])
+print(s[-9])
+print(s[-10])
+print('hii.............')
+
+g="kjcucbuybwefbuAybscEWBC"
+print(len(s))
+print(len(g))
+
+
+#........class 11
+
+s='coffee'
+t='bread'
+print(s[1])
+print(s[1:3])
+print(s[1:5])
+print(s[0:5])
+
+
+s='0283478922'   #string not a integer
+a=s[2]  #string 
+b=s[3]  #string 
+print(a)
+print(b)
+print(a+b)
+#   print(a*b)   not perform because it is a string not a integer
+
+# but once we convert it into integer then we perform operatora
+
+s='0283478922'
+a=int(s[2])   #conveted to integer
+b=int(s[3])  #conveted to integer
+print(a)
+print(b)
+print(a+b)
+print(a*b)
+
+
+
+
+#....class 10
+
+print("hello")
+
+#Atrithhetic(+,-,/,//,%,**)
+
+print('addition',2+2)
+print("substraction", 9-2)
+print("multiplication", 2*4)
+print("division", 9/3)
+
+print('Floor division', 7//3)
+print('mudulus', 7%3)
+print('Exponenttiation', 6**2)
+
+
+#   Relational (<,>,>=,<=,==,!=)
+#grater and lesser to
+print(5>10)
+print(5<10)
+#grater equal to and lesser equal to 
+print(5>=10)
+print(5<=5)
+#equal to eqaul
+print(5==5)
+print(5==50)
+#not equal to
+print(5!=50)
+print(5!=5)
+
+# logical (and ,or, not)
+
+print(True and False)
+print(True and True)
+print(False and False)
+print(False and True)
+
+print(not (True))
+print(not False)
+print(not True)
+
+
+#........class 10b
+
+n=10*23+12
+print(n)
+
+#.......class 10a
+
+i=10
+f=23.3
+b="India"
+p=False
+
+print(type(i))
+print(type(f))
+print(type(b))
+print(type(p))
+
+a=int(4.3)
+c=int('10')
+print(a,type(a))
+print(c, type(c))
+
+d=float(4)
+g=float('10')
+print(d,type(d))
+print(g,type(g))
+
+h=bool(12.3)
+k=bool(0.0)
+o=bool(-2.3)
+e=bool('0')
+
+print(h,type(h))
+print(k,type(k))
+print(o,type(o))
+print(e,type(e))
+
+#.........class 9
+
+n=10
+print(n)
+r=4.3
+print(r)
+s="Viaby"
+print(s)
+
+print(type(n))
+print(type(r))
+print(type(s))
+
+#.........class 7 and 8
+
+l=[10,20,30.34,45,24]
+print(l[0])
+print(l[1])
+print(l[2])
+print(type(l))
+
+
+#.............class 6 
+
+r=int(input("Enter the radius of circle: "))
+area=3.14*r*r
+print("area of the circle ",r,"is",area)
+
+#............Class 5c
+n=str(input("type your name: "))
+p=str(input("Type your location: k"))
+print("Hello",n,"how is the weather in",p,"?")
+age=int(input("what is your age: "))
+print("Good to know that you are",age,"year old.")
+
+#.........class 5b
+
+print("hello,type in your name")
+n=str(input())
+print("which place you are in?")
+p=str(input())
+print("hello",n,"How is the weather in",p,"?")
+print("what is your age?")
+age=int(input())
+print("Good to know you are",age,"years old.")
+
+#...........Class 5a
+print("Enter a number:")
+n=int(input())
+print(n+1)
+print(n+2)
+
+#.......Class 4 b
+a=12
+print(a)
+a=a+1
+print(a)
+a=a+1
+print(a)
+print("a")
+b=20
+print(a+b)
+
+#.......Class 4 a
+print("Hello")
+print("Namaste India")
+print("Thsi is the second line...")
+print("****")
+print("I dont know what to typing")
+print("     *")
+print("    **")
+print("   ***")
+print("  ****")
+print('hello world',"hello india","Namaste India")
+print('10')
+print(23.4)
+print("Hello world",10,23.4)
+print("He")
+
+
